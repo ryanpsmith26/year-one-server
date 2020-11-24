b09e15953a84d7a0aba4bee76097bdac5ae697f7
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-11-24 13:24:49 -0600

    adds GET route for existing movies in DB when searching, adds PUT routes for thumbs up and down

--- api/movie.js
+++ api/movie.js
@@ -1,20 +1,62 @@
 const router = require('express').Router();
 const { Movie } = require('../db');
 
-// GET /api/movies
-router.get('/', async (req, res, next) => {
+// GET api/movies/:imdbId -- get vote data if exists in DB
+router.get('/:imdbId', async (req, res, next) => {
 	try {
-		const movies = await Movie.findAll();
-		res.json(movies);
+		const movie = await Movie.findOne({
+			where: {
+				imdbId: req.params.imdbId
+			}
+		});
+		if (movie) res.json(movie);
+		else res.json({ thumbsUp: 0, thumbsDown: 0 });
 	} catch (error) {
 		next(error);
 	}
 });
 
-// POST /api/movies
-router.post('/', async (req, res, next) => {
+// PUT /api/movies/thumbsUp
+router.put('/thumbsUp', async (req, res, next) => {
 	try {
-		await Movie.findOrCreate({});
+		const [ movie, created ] = await Movie.findOrCreate({
+			where: {
+				imdbId: req.body.imdbId
+			},
+			defaults: {
+				title: req.body.title,
+				thumbsUp: 1,
+				thumbsDown: 0
+			}
+		});
+		if (!created) {
+			await movie.thumbsUp++;
+			await movie.save();
+		}
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// PUT /api/movies/thumbsDown
+router.put('/thumbsDown', async (req, res, next) => {
+	try {
+		const [ movie, created ] = await Movie.findOrCreate({
+			where: {
+				imdbId: req.body.imdbId
+			},
+			defaults: {
+				title: req.body.title,
+				thumbsUp: 0,
+				thumbsDown: 1
+			}
+		});
+		if (!created) {
+			await movie.thumbsDown++;
+			await movie.save();
+		}
+		res.sendStatus(204);
 	} catch (error) {
 		next(error);
 	}
--- db/movie.js
+++ db/movie.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('./database');
 
 const Movie = db.define('movie', {
+	imdbId: Sequelize.STRING,
 	title: Sequelize.STRING,
 	thumbsUp: Sequelize.INTEGER,
 	thumbsDown: Sequelize.INTEGER
