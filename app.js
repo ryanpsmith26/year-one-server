185187eb1bf0fab191db4089db07684554979bf7
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-12-03 10:40:19 -0500

    edits res headers for all reqs with app USE to fix CORS error

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// for CORS error running react app and express server both locally
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 // mount main router
 app.use('/api', require('./api'));
 
